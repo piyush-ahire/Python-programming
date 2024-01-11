8e448f6516198dc41d66708b5d354338fa917488
piyush-ahire <piyushahire402@gmail.com>
2024-01-11 20:43:12 +0530

    new modules added

--- 0test.py
+++ 0test.py
@@ -1,12 +1,156 @@
 # Guido van Rossum invented in netherlands 1991 interpreted and open source lang, scalable, object oriented 
 
-print("hello")
-
-def hello():
-    print("world")
 
 
-hello()
+# no need to declare data type, it will depends on the content in it
+# name = "hello"
 
-print("hey")
-   
+# A = 10
+# a = 5.5
+
+# print(name)
+# print(A)
+# print(a)
+
+# # to check data type
+# print(type(name))
+
+
+
+# name = 10.5
+
+# print(type(name)) # <class 'float'>
+
+# #single line string
+# a = 'asdsad sadasadas'
+
+# #multiline string
+# b = "asada ssadsa\
+#     fdfdsfdsfdsfds\
+#         dfdsfdsfdsfds"
+
+# # withot \ multiline string
+# c='''ffdfdd dfdsfd 
+# dfdsfd
+# dfdfdsf'''
+
+
+# # four literals
+# #string, numeric, boolean, none
+
+# # string : formed by enclosing a text in it
+
+# s = 4
+# d = 6
+# print(s<d)
+
+
+
+#changing data type from one to another
+
+# a = 10.7
+# print(type(a))   # <class 'int'>
+
+# # b = input("Enter: ") # always string *IMP
+# # b = int(input("Enter: ")) # always int *IMP
+# # print(type(b))
+
+# result = int(a) # type cast
+# print(type(result))
+# print("Result is: ",result)
+
+# c = 4
+# f = 6
+# print("Addition is:", c+f)
+
+
+
+
+
+#if-else maintain indentation
+
+# age = 17
+# b = 5
+
+# if age>=18:
+#     print("You are eligible for voting")
+# else:
+#     print("Not eligible")
+
+
+#relational operator
+
+# a = 4
+# b = 4
+
+# print(a == b)
+
+
+# creation of function
+# def abc():
+#     print("hello")
+
+# abc()
+
+
+#logical operator
+#and , or , not
+# a = 5
+# b = a>4 and a>2
+
+# print(b)
+
+# #not
+# c = 4
+# d = not(c>7)
+# print(d)
+# print(a!=c)
+
+
+
+#in membership operator
+
+# a = ["suresh", "Ramesh"]
+# print('Ramesh' in a)
+
+
+#for loop indentation mandatory
+# a = ["suresh", "Ramesh",10]
+
+# for i in a:
+#     print(i)
+
+
+# slicing
+
+# a = "Hello world"
+# print(a[2:5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
