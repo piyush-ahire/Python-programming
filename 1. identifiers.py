e967686f87da9b76b721bcb302b1795430866b84
piyush-ahire <piyushahire402@gmail.com>
2024-01-12 18:15:08 +0530

    Module 1 added

--- 1. identifiers.py
+++ 1. identifiers.py
@@ -1,1 +1,23 @@
+# ***Identifier*** : 
+
+# A Python identifier is a name used to identify a variable, function, class, module or other object.
+
+# Example : 
+
+name = "Hello"
+
+A = 10
+
+a = 5.5
+
+print(name)
+print(A)
+print(a)
+
+
+
+
+
+
+
 
